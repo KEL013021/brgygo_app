6f8c803129b4bb231499c6e57d1e8f49a2b9590b
Chris Michael Macalalad Leynes <22-70677@g.batstate-u.edu.ph>
2025-07-29 17:16:22 +0800

    Delete BRGY/BRGYGO directory

--- BRGY/BRGYGO/database/announcement_get.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connection.php');
-$id = $_GET['id'];
-$result = $conn->query("SELECT * FROM announcements WHERE id = $id");
-$row = $result->fetch_assoc();
-echo json_encode($row);
-?>
